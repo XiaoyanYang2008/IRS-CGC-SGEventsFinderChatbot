4c158633f9d271fccc1c21b4de7ebd55b1f15057
tealeeseng <tealeeseng@gmail.com>
2019-08-06 13:28:26 +0800

    api.eventfinda.sg python3 sample data. seems like random reply for given URL.

--- api-test/eventfindaTest.py
+++ api-test/eventfindaTest.py
@@ -1,5 +1,8 @@
 '''
 sample code from https://www.eventfinda.sg/api/v2/overview#python
+
+https://www.eventfinda.sg/api/v2/events
+
 '''
 
 # import json, urllib2, base64
@@ -8,7 +11,11 @@ import json
 
 from urllib.request import urlopen, Request
 
-url = 'http://api.eventfinda.sg/v2/events.json?rows=2'
+# url = 'http://api.eventfinda.sg/v2/events.json?rows=5'  # ok
+# url = 'http://api.eventfinda.sg/v2/events.json?category_slug=children-kids'  #ok.
+url = 'http://api.eventfinda.sg/v2/events.json?free=1'  # ok.
+# url = 'http://api.eventfinda.sg/v2/events.json?q=(children)'  # ok
+
 username = 'schoolprojectgoogleassistantchatbotforsingaporeevents';
 password = 'qbvnn9ynhbt3';
 request = Request(url)

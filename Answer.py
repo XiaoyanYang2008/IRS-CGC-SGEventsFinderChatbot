d71639107c4492d62bdd1be4e8e05d080d1a2133
t-rkr <tarunrulz11@gmail.com>
2019-08-24 09:13:59 +0530

    Added interface for displaying results in 1. List \n 2. Basic Card. Ref app.py on construct.

--- Answer.py
+++ Answer.py
@@ -18,15 +18,76 @@ class Answer:
 
     def add_google_payload(self):
         if not 'payload' in self.reply.keys():
-            self.reply['payload'] = {'google': {
-                "expectUserResponse": "true"
-                # "isSsml": "false",
-                # "noInputPrompts": [],
-                # 'richResponse':{
-                #     "items":[]
-                #     }
+            self.reply['payload'] = {
+                'google': {
+                    "expectUserResponse": "true"
+                }
             }
+
+    def add_richResponse(self):
+        self.reply['payload']['google']['richResponse'] = {"items": []}
+
+    def add_richResponseItem(self, richResponseItem):
+        self.reply['payload']['google']['richResponse']['items'].append(richResponseItem)
+
+    def create_simpleResponse(self, responseText):
+        return {
+            "simpleResponse": {
+                "textToSpeech": responseText
             }
+        }
+
+    def create_basicCard(self, title, subtile, formattedText, image, buttons, imageDisplayOptions="CROPPED"):
+        return {
+            "basicCard": {
+                "title": title,
+                "subtitle": subtile,
+                "formattedText": formattedText,
+                "image": image,
+                "buttons": [buttons],
+                "imageDisplayOptions": imageDisplayOptions
+            }
+        }
+
+    def create_basicCardButton(self, title, url):
+        return {
+            "title": title,
+            "openUrlAction": {
+                "url": url
+            }
+        }
+
+    def add_systemIntent(self):
+        self.reply['payload']['google']['systemIntent'] = {
+            "intent": "actions.intent.OPTION",
+            "data": {
+                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec"
+            }
+        }
+
+    def add_listSelect(self, title, items):
+        self.reply['payload']['google']['systemIntent']['data']['listSelect'] = {
+            "title": title,
+            "items": items
+        }
+
+    def create_listItems(self, op_key, title, description, image, op_synonyms=[]):
+        # Perhaps directly add items ? v2 maybe
+        return {
+            "optionInfo": {
+                "key": op_key,
+                "synonyms": op_synonyms
+            },
+            "description": description,
+            "image": image,
+            "title": title
+        }
+
+    def create_richImage(self, url, hoverText):
+        return {
+            "url": "https:" + url,
+            "accessibilityText": hoverText
+        }
 
     def add_suggestsions(self, suggestion):
         if not 'suggestions' in self.reply['payload']['google']['richResponse'].keys():
@@ -34,68 +95,35 @@ class Answer:
         else:
             self.reply['payload']['google']['richResponse']['suggestions'].append(suggestion)
 
-    def add_payload1(self, suggestion, item):
-        if not 'payload' in self.reply.keys():
-            self.reply["payload"] = {
-                "google": {
-                    "expectUserResponse": "true",
-                    "isSsml": "false",
-                    "noInputPrompts": [],
-                    "richResponse": {
-                        'items': [{
-                            "simpleResponse": {
-                                "textToSpeech": "Here are some results for you"
-                            }
-                        }],
-                        'suggestions': []
-                    },
-                    "systemIntent": {
-                        "intent": "actions.intent.OPTION",
-                        "data": {
-                            "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                            "carouselSelect": {"items": []}
-                        }
-                    }
-                }
-            }
-        self.reply['payload']['google']['richResponse']['suggestions'].append({"title": suggestion if len(suggestion) <
-                                                                                                      23 else suggestion[
-                                                                                                              :22] + ".."})
-        self.reply['payload']['google']['systemIntent']['data']['carouselSelect']['items'].append(item)
-
-    def add_BrowseCarouselItem(self):
-        # self.reply['payload']['google']['systemIntent']['data']['']
-        pass
-
-    def create_BrowseCarouselItem(self, name, url, description, image):
-        pass
-
-    def create_card(self, title, card_text, image_url, button=None):
-        if button:
-            return {"card": {
-                "title": title,
-                "subtitle": card_text,
-                "imageUri": image_url,
-                "buttons": button
-            }}
-        return {"card": {
-            "title": title,
-            "subtitle": card_text,
-            "imageUri": image_url
-        }}
-
-    def create_item(self, name, description, image_url):
-        return {"optionInfo": {
-            "key": name,
-            "synonyms": [
-                name
-            ]
-        },
-            "description": description,
-            "image": {"url": "https:" + image_url, 'accessibilityText': name},
-            "title": name
-        }
-
+    #
+    # def add_payload1(self, suggestion, item):
+    #     if not 'payload' in self.reply.keys():
+    #         self.reply["payload"] = {
+    #             "google": {
+    #                 "expectUserResponse": "true",
+    #                 "isSsml": "false",
+    #                 "noInputPrompts": [],
+    #                 "richResponse": {
+    #                     'items': [{
+    #                         "simpleResponse": {
+    #                             "textToSpeech": "Here are some results for you"
+    #                         }
+    #                     }],
+    #                     'suggestions': []
+    #                 },
+    #                 "systemIntent": {
+    #                     "intent": "actions.intent.OPTION",
+    #                     "data": {
+    #                         "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+    #                         "carouselSelect": {"items": []}
+    #                     }
+    #                 }
+    #             }
+    #         }
+    #     self.reply['payload']['google']['richResponse']['suggestions'].append({"title": suggestion if len(suggestion) <
+    #                                                                                                   23 else suggestion[
+    #                                                                                                           :22] + ".."})
+    #     self.reply['payload']['google']['systemIntent']['data']['carouselSelect']['items'].append(item)
     def add_outputContexts(self, name, lifespanCount, parameters):
         if 'outputContexts' in self.reply.keys():
             self.reply['outputContexts'].append({
@@ -183,39 +211,73 @@ class Answer:
 
     def test_basicCard(self):
         self.reply['payload']['google'] = {"richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "Here's an example of a basic card."
-            }
-          },
-          {
-            "basicCard": {
-              "title": "Title: this is a title",
-              "subtitle": "This is a subtitle",
-              "formattedText": "This is a basic card.  Text in a basic card can include \"quotes\" and\n    most other unicode characters including emoji 📱.  Basic cards also support\n    some markdown formatting like *emphasis* or _italics_, **strong** or\n    __bold__, and ***bold itallic*** or ___strong emphasis___ as well as other\n    things like line  \nbreaks",
-              "image": {
-                "url": "https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png",
-                "accessibilityText": "Image alternate text"
-              },
-              "buttons": [
+            "items": [
                 {
-                  "title": "This is a button",
-                  "openUrlAction": {
-                    "url": "https://assistant.google.com/"
-                  }
+                    "simpleResponse": {
+                        "textToSpeech": "Here's an example of a basic card."
+                    }
+                },
+                {
+                    "basicCard": {
+                        "title": "Title: this is a title",
+                        "subtitle": "This is a subtitle",
+                        "formattedText": "This is a basic card.  Text in a basic card can include \"quotes\" and\n    most other unicode characters including emoji 📱.  Basic cards also support\n    some markdown formatting like *emphasis* or _italics_, **strong** or\n    __bold__, and ***bold itallic*** or ___strong emphasis___ as well as other\n    things like line  \nbreaks",
+                        "image": {
+                            "url": "https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png",
+                            "accessibilityText": "Image alternate text"
+                        },
+                        "buttons": [
+                            {
+                                "title": "This is a button",
+                                "openUrlAction": {
+                                    "url": "https://assistant.google.com/"
+                                }
+                            }
+                        ],
+                        "imageDisplayOptions": "CROPPED"
+                    }
+                },
+                {
+                    "simpleResponse": {
+                        "textToSpeech": "Which response would you like to see next?"
+                    }
                 }
-              ],
-              "imageDisplayOptions": "CROPPED"
-            }
-          },
-          {
-            "simpleResponse": {
-              "textToSpeech": "Which response would you like to see next?"
-            }
-          }
-        ]
-      }
-    }
+            ]
+        }
+        }
+
     def add_followupEventInput(self, name, parameters, languageCode="en-US"):
         self.reply["followupEventInput"] = dict(name=name, languageCode=languageCode, parameters=parameters)
+
+    def create_card(self, title, card_text, image_url, button=None):
+        if button:
+            return {"card": {
+                "title": title,
+                "subtitle": card_text,
+                "imageUri": image_url,
+                "buttons": button
+            }}
+        return {"card": {
+            "title": title,
+            "subtitle": card_text,
+            "imageUri": image_url
+        }}
+
+    def create_item(self, name, description, image_url):
+        return {"optionInfo": {
+            "key": name,
+            "synonyms": [
+                name
+            ]
+        },
+            "description": description,
+            "image": {"url": "https:" + image_url, 'accessibilityText': name},
+            "title": name
+        }
+
+    def add_BrowseCarouselItem(self):
+        # self.reply['payload']['google']['systemIntent']['data']['']
+        pass
+
+    def create_BrowseCarouselItem(self, name, url, description, image):
+        pass
--- app.py
+++ app.py
@@ -21,49 +21,46 @@ def main():
     dialog_session = dialog_request['session']
     print(dialog_intent)
     print(dialog_request)
-    if dialog_intent == 'events.search':
-        pass
+
     params = parse_params(dialog_intent, dialog_request)
     if params:
         search_query = dialog_request['queryResult']['parameters'][params[0]]
     else: search_query = "marathon"
 
     #Lots of processing
-    answer = Answer.Answer()
-    cards = []
-    items = []
-    suggestions = []
-    api_responses = eventfinda_api.query_ef_api(search_query)
-    #answer.add_followupEventInput("eventsearch-followup",[])
-    if api_responses:
-        answer.add_google_payload()
-        answer.test_list()
-        #test answer
-        #----------------------------------------------------------------------------------#
-        # handbreak = 0
-        # for event in api_responses:
-        #     name = event['name']
-        #     description = event['description']
-        #     image_url = event['images']['images'][0]['transforms']['transforms'][3]['url']
-        #     button = [{"text": "button text","postback": "http://assistant.google.com/"}]
-        #     card = answer.create_card(title=name,card_text=description,image_url=image_url)
-        #     item = answer.create_item(name,description,image_url)
-        #     suggestion = name
-        #     #print(card)
-        #     if handbreak < 3:
-        #         answer.add_fulfillmentMessage(card)
-        #         answer.add_payload(suggestion,item)
-        #         handbreak = handbreak + 1
-        # ----------------------------------------------------------------------------------#
-        #answer.reply.pop('fulfillmentText')
+    if dialog_intent == 'events.search':
+        answer = Answer.Answer()
+        api_responses = eventfinda_api.query_ef_api(search_query)
+        #answer.add_followupEventInput("eventsearch-followup",[])
+        if api_responses:
+            answer.add_google_payload()
+            answer.add_richResponse()
+            answer.add_richResponseItem(answer.create_simpleResponse("Here are your results"))
+            answer.add_systemIntent()
+            items = []
+            handbreak = 5
+            for event in api_responses:
+                name = event['name']
+                description = event['description']
+                image_url = event['images']['images'][0]['transforms']['transforms'][3]['url']
 
-    #print(answer.reply)
+                if handbreak > 0:
+                    items.append(answer.create_listItems(name,name,description,answer.create_richImage(image_url,name)))
+                    handbreak = handbreak - 1
+            answer.add_listSelect("Query Results",items)
+        print(answer.reply)
+        return Response(json.dumps(answer.reply), status=200, content_type="application/json")
+
     if dialog_intent == "events.search - custom":
         answer = Answer.Answer()
         answer.add_google_payload()
-        answer.test_basicCard()
+        answer.add_richResponse()
+        answer.add_richResponseItem(answer.create_simpleResponse("Okay..."))
+        basicCard = answer.create_basicCard("Hello","Hello World!","Ipsum lorme bleh  Okay one two three \\ four five six seven.",answer.create_richImage("//cdn.eventfinda.sg/uploads/events/transformed/42063-19086-27.jpg","image"),answer.create_basicCardButton("Click Me","https://google.com"))
+        answer.add_richResponseItem(basicCard)
+        #answer.add_systemIntent()
         return Response(json.dumps(answer.reply), status=200, content_type="application/json")
-    else:
-        return Response(json.dumps(answer.reply), status=200, content_type="application/json")
+    # else:
+    #     return Response(json.dumps(answer.reply), status=200, content_type="application/json")
 
 
